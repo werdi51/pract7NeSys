bb22ecbe21353844e3671fe0ff51ba41558cb48f
werdi51 <spotifig2@gmail.com>
2025-11-03 14:51:52 +0500

    Add files via upload

---
+++ pract7correct/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract7correct
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User CurrentUser = new User();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ResetUser(object sender, RoutedEventArgs e)
+        {
+            var currentUser = (User)Resources["CurrentUser"];
+            currentUser.Id = 0;
+            currentUser.Name = "Имя пользователя";
+            currentUser.Email = "user@example.com";
+            currentUser.Age = 18;
+        }
+    }
+}
---
+++ pract7correct/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract7correct
+{
+    class User : INotifyPropertyChanged
+    {
+        private int _id = 0;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _name = "Имя пользвоателя";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _email = "user@example.com";
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private int _age = 18;
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value != _age)
+                {
+                    _age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
